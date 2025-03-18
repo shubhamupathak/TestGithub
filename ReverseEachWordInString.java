a965dd4c9fb742d2ef002b11b7b511315acbe75e
shubhamupathak <shubhamupathak@gmail.com>
2025-03-18 14:04:26 +0530

    added a new java file for reverse each word in string.

---
+++ ReverseEachWordInString.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class ReverseEachWordInString 
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+
+        String str = input.nextLine();
+        String reverse = "";
+
+        String words[] = str.split(" ");
+        for(String wordstr : words)
+        {
+            String Word = wordstr;
+            char ch;
+            String empstr = "";
+            for(int i = 0; i < Word.length(); i++)
+            {
+                ch = Word.charAt(i);
+                empstr = ch + empstr;
+            }
+
+            reverse = reverse+empstr+" ";
+        }
+
+        System.out.println(reverse);
+    }
+    
+}
